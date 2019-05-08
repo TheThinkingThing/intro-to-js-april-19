c02d11249b779a20b8888c3ae999144b5cdbb1b8
Andrew <reyes.andrew.94@outlook.com>
2019-05-07 20:13:03 -0700

    changed to immediately invoked function

--- vinson/Andrew.js
+++ vinson/Andrew.js
@@ -1,6 +1,7 @@
-const container = document.createElement('div');
+(function(){
+const container = document.createElement(tagName: 'div');
 document.body.appendChild(container);
 container.style['background-color'] = 'blanchedAlmond';
 container.style['height'] = '30vh';
 container.innerText = 'This is Andrew\'s box.';
-
+})();
