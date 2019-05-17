aa668af923ac68757f529fafaf14eef5382a3c08
Andrew <reyes.andrew.94@outlook.com>
2019-05-16 18:39:19 -0700

    add html page drawjs

--- vinson/Andrew.js
+++ vinson/Andrew.js
@@ -1,7 +1,10 @@
 (function(){
-const container = document.createElement(tagName: 'div');
+const container = document.createElement('div');
 document.body.appendChild(container);
 container.style['background-color'] = 'blanchedAlmond';
-container.style['height'] = '30vh';
+container.style['height'] = '700px';
+container.style.margin = '5px';
+container.style.backgroundImage = 'url("https://previews.123rf.com/images/haryfoto/haryfoto1706/haryfoto170600123/80324341-bamboo-texture-wood-background-bamboo-plank-backdrop-wallpaper.jpg")';
+container.style.backgroundImage = 'overflow:hidden';
 container.innerText = 'This is Andrew\'s box.';
 })();
